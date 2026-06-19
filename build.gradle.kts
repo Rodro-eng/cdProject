2b8af59b867d9939c5a0315438993b70116a0611
Rodro52 <rodrosahar02@gmail.com>
2026-06-19 22:26:54 +0600

    Fix: configuration name is 'cloudstream' not 'apk'

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,23 +64,21 @@ subprojects {
         }
     }
 
-    afterEvaluate {
-        dependencies {
-            val implementation by configurations
-            val apk by configurations
+    dependencies {
+        val implementation by configurations
+        val cloudstream by configurations
 
-            // Stubs for all Cloudstream classes
-            apk("com.lagradost:cloudstream3:pre-release")
+        // Stubs for all Cloudstream classes
+        cloudstream("com.lagradost:cloudstream3:pre-release")
 
-            // these dependencies can include any of those which are dependencies of cloudstream3
-            // so they don't need to be included in the apk
-            // see https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-            implementation(kotlin("stdlib")) // adds standard library
-            implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
-            implementation("org.jsoup:jsoup:1.18.3") // html parser
-            implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
-            implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-        }
+        // these dependencies can include any of those which are dependencies of cloudstream3
+        // so they don't need to be included in the apk
+        // see https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
+        implementation(kotlin("stdlib")) // adds standard library
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
+        implementation("org.jsoup:jsoup:1.18.3") // html parser
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     }
 
     cloudstream {
