2d10ccd3d6143030d4e4cefb5e833613d975251b
Rodro52 <rodrosahar02@gmail.com>
2026-06-19 23:10:16 +0600

    Fix: Actually remove AppCompatActivity and Context casting from Plugins

--- LowSpeed/src/main/kotlin/com/example/LowSpeedPlugin.kt
+++ LowSpeed/src/main/kotlin/com/example/LowSpeedPlugin.kt
@@ -1,16 +1,12 @@
 package com.example
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class LowSpeedPlugin : Plugin() {
-    private var activity: AppCompatActivity? = null
-
     override fun load(context: Context) {
-        activity = context as AppCompatActivity
         registerMainAPI(LowSpeedProvider())
     }
 }
