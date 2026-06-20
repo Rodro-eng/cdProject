f41e76567afae1f8360c7bf1f04127154f4c1c35
Rodro52 <rodrosahar02@gmail.com>
2026-06-20 20:53:26 +0600

    Add SkyStream extension - Indian Live TV with 500+ channels

---
+++ SkyStream/build.gradle.kts
@@ -0,0 +1,17 @@
+// Use an integer for version numbers
+version = 1
+
+cloudstream {
+    description = "Indian Live TV - 500+ channels across Sports, Entertainment, Movies, News, Kids, Music, Devotional & more"
+    authors = listOf("Rodro-eng")
+
+    status = 1
+
+    tvTypes = listOf(
+        "Live",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=go.skym3u.top&sz=%size%"
+
+    language = "hi"
+}
